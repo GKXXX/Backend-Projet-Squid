ae35fb840a5393dc909478a351fe7c8a57ee7ef1
GKXXX <killiangassin78@gmail.com>
2022-05-20 12:35:49 +0200

    finition endpoints

--- src/main/java/com/example/testsqljppptn/controllers/OrderController.java
+++ src/main/java/com/example/testsqljppptn/controllers/OrderController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/order")
@@ -17,6 +19,16 @@ public class OrderController {
     @GetMapping()
     public Iterable<Order> getAllOrders() {
         return orderRepository.findAll();
+    }
+
+    @GetMapping("/byId")
+    public ResponseEntity<Optional<Order>> getOrder(@RequestParam Long id) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isPresent()) {
+            return ResponseEntity.ok(order);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping()
